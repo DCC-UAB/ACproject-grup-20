6386ffe157beca6b1e0cad99b795354e2f763b13
MartiVC <martiverges6@gmail.com>
2024-11-28 14:20:38 +0100

    veure datasets

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-#arixu def
 import torch
+
+import os
+
+# Ruta de la carpeta 'data'
+data_folder = './data'
+
+# Comprovar si la carpeta existeix
+if os.path.exists(data_folder):
+    # Llistar els fitxers dins de la carpeta
+    files = os.listdir(data_folder)
+    print("Fitxers dins de la carpeta 'data':")
+    for file in files:
+        print(f"- {file}")
+else:
+    print(f"La carpeta {data_folder} no existeix.")
