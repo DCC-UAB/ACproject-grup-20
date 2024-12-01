1c107fcf090b9261bfc44e051a4db7bd9d57c625
Almoujtaba <91468363+Almoujtaba@users.noreply.github.com>
2024-12-01 10:17:29 +0100

    Update Detector_idiomas.py
    Tenemos 3 idiomas a parte del ingles que son poco significativos

--- Detector_idiomas.py
+++ Detector_idiomas.py
@@ -1,21 +1,24 @@
+import pandas as pd
 import langid
 
-# Lista de frases para detectar el idioma
-frases = [
-    'Good evening, I hope you had a great day',
-    'Hoy es un día perfecto para aprender algo nuevo',
-    'Bon dia, com estàs avui?',
-    'Bonjour, je suis heureux de vous rencontrer',
-    'Guten Morgen, wie geht es Ihnen?'
-]
+# Función para detectar idiomas
+def detect_languages(data, column_name):
+    detected_languages = data[column_name].apply(lambda x: langid.classify(x)[0])
+    return detected_languages.value_counts()
 
-# Diccionario de idiomas para traducir los códigos a nombres de idiomas
-idiomas = {'ca': 'català', 'es': 'castellà', 'en': 'anglès', 'fr': 'francès', 'de': 'alemany'}
 
-# Detectar y mostrar resultados para cada frase
-for frase in frases:
-    idioma, _ = langid.classify(frase)  
-    idioma_nombre = idiomas.get(idioma, 'Idioma desconocido')
-    print(f"Frase: {frase}")
-    print(f"Idioma detectado: {idioma} ({idioma_nombre})")
-    print()
+test_data = pd.read_csv('C:/Users/Almoujtaba/Desktop/CARRERA/ACproject-grup-20/datasets_AC/Test.csv')
+train_data = pd.read_csv('C:/Users/Almoujtaba/Desktop/CARRERA/ACproject-grup-20/datasets_AC/Train.csv')
+valid_data = pd.read_csv('C:/Users/Almoujtaba/Desktop/CARRERA/ACproject-grup-20/datasets_AC/Valid.csv')
+
+# Detectar idiomas en cada archivo
+test_languages = detect_languages(test_data, "text")
+train_languages = detect_languages(train_data, "text")
+valid_languages = detect_languages(valid_data, "text")
+
+#Resultados
+print("Idiomas en Test.csv:", test_languages)
+print("Idiomas en Train.csv:", train_languages)
+print("Idiomas en Valid.csv:", valid_languages)
+
+####Casi todas son en ingles, 3 que no son.: italiano, noruego y indonesia.
