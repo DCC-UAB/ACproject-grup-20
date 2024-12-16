2725d9d3c3eb90d17f83eb091e558050f006e86a
MartiVC <martiverges6@gmail.com>
2024-12-16 02:20:41 +0100

    docu dataanalisis

--- kharrat_SVM_RF/Detector_idiomas.py
+++ kharrat_SVM_RF/Detector_idiomas.py
@@ -1,24 +1,30 @@
 import pandas as pd
 import langid
 
-# Función para detectar idiomas
+# Funció per detectar idiomes
 def detect_languages(data, column_name):
     detected_languages = data[column_name].apply(lambda x: langid.classify(x)[0])
-    return detected_languages.value_counts()
+    return detected_languages
 
+# Llegir les dades
+test_data = pd.read_csv('C:/Users/marti/OneDrive/Escriptori/datasets_AC/Test.csv')
+train_data = pd.read_csv('C:/Users/marti/OneDrive/Escriptori/datasets_AC/Train.csv')
+valid_data = pd.read_csv('C:/Users/marti/OneDrive/Escriptori/datasets_AC/Valid.csv')
 
-test_data = pd.read_csv('C:/Users/Almoujtaba/Desktop/CARRERA/ACproject-grup-20/datasets_AC/Test.csv')
-train_data = pd.read_csv('C:/Users/Almoujtaba/Desktop/CARRERA/ACproject-grup-20/datasets_AC/Train.csv')
-valid_data = pd.read_csv('C:/Users/Almoujtaba/Desktop/CARRERA/ACproject-grup-20/datasets_AC/Valid.csv')
-
-# Detectar idiomas en cada archivo
+# Detectar idiomes en cada arxiu
 test_languages = detect_languages(test_data, "text")
 train_languages = detect_languages(train_data, "text")
 valid_languages = detect_languages(valid_data, "text")
 
-#Resultados
-print("Idiomas en Test.csv:", test_languages)
-print("Idiomas en Train.csv:", train_languages)
-print("Idiomas en Valid.csv:", valid_languages)
+# Filtrar les línies que no són en anglès
+test_non_english = test_data[test_languages != 'en']
+train_non_english = train_data[train_languages != 'en']
+valid_non_english = valid_data[valid_languages != 'en']
 
-####Casi todas son en ingles, 3 que no son.: italiano, noruego y indonesia.
+# Mostra els resultats
+print("Línies no en anglès a Test.csv:")
+print(test_non_english[['text']])
+print("Línies no en anglès a Train.csv:")
+print(train_non_english[['text']])
+print("Línies no en anglès a Valid.csv:")
+print(valid_non_english[['text']])
